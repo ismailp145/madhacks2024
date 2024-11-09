012f65fdcf147402369a95f038d3312bd668513b
Aditya YV <adityayv802@gmail.com>
2024-11-09 11:54:49 -0600

    Scraper and parellel process files

---
+++ master.py
@@ -0,0 +1,46 @@
+import Scraper
+import Slave
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+    
+def scatter_plot(X, y, blockProgram = False):
+    plt.figure()
+    plt.scatter(X, y)
+
+    # if labels is not None:
+    #     for i, label in enumerate(labels):
+    #         plt.annotate(label, (X[i], y[i]))
+    
+    plt.show(block=blockProgram)
+    plt.close()
+    
+def plot_graph(X, y, blockProgram = False):
+    plt.plot(X, y)
+    plt.show(block = blockProgram)
+    plt.close()
+
+if __name__ == '__main__':
+    print("Starting")
+    subject = "Wikipedia"
+    result = "Paris"
+
+    #Scraping the data
+    start = time.time()
+    uarr = Scraper.startScraping(subject, result)
+    end = time.time()
+    print("Time taken to scrape",subject,": ", round(end-start, 3),"seconds")
+
+    #Processing weights
+
+    start = time.time()
+    #Extracting number of links from each page
+    output = Slave.main(uarr)
+    end = time.time()
+    print("Time taken: ", round(end-start, 3),"seconds")
+
+    #Plot data
+    # scatter_plot(range(0, len(wY)), wY)
+    # scatter_plot(range(0,len(uarr)), output)
+    # scatter_plot(output, wY, True)
